5a8b1edbbde092cb9ff044dcff47ab18f464d0f6
LiamKT0910 <bigboy09101994@gmail.com>
2023-01-18 12:50:10 -0600

    commit

---
+++ index.js
@@ -0,0 +1,318 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const PlAYER_STORAGE_KEY = "F8_PLAYER";
+
+const player = $(".player");
+const cd = $(".cd");
+const heading = $("header h2");
+const cdThumb = $(".cd-thumb");
+const audio = $("#audio");
+const playBtn = $(".btn-toggle-play");
+const progress = $("#progress");
+const prevBtn = $(".btn-prev");
+const nextBtn = $(".btn-next");
+const randomBtn = $(".btn-random");
+const repeatBtn = $(".btn-repeat");
+const playlist = $(".playlist");
+
+const app = {
+  currentIndex: 0,
+  isPlaying: false,
+  isRandom: false,
+  isRepeat: false,
+  config: {},
+  // (1/2) Uncomment the line below to use localStorage
+  // config: JSON.parse(localStorage.getItem(PlAYER_STORAGE_KEY)) || {},
+  songs: [
+    {
+      name: "Oh Well",
+      singer: "G-Eazy",
+      path: "https://music.youtube.com/watch?v=Q7c9QoHgsMw&feature=share",
+      image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DQ7c9QoHgsMw&psig=AOvVaw0sFpL8xDRuLyozBVeFUTf5&ust=1668578176690000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJizjZ7Ar_sCFQAAAAAdAAAAABAE"
+    },
+    {
+      name: "Tu Phir Se Aana",
+      singer: "Raftaar x Salim Merchant x Karma",
+      path: "https://mp3.vlcmusic.com/download.php?track_id=34213&format=320",
+      image:
+        "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg"
+    },
+    {
+      name: "Naachne Ka Shaunq",
+      singer: "Raftaar x Brobha V",
+      path:
+        "https://mp3.filmysongs.in/download.php?id=Naachne Ka Shaunq Raftaar Ft Brodha V Mp3 Hindi Song Filmysongs.co.mp3",
+      image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg"
+    },
+    {
+      name: "Mantoiyat",
+      singer: "Raftaar x Nawazuddin Siddiqui",
+      path: "https://mp3.vlcmusic.com/download.php?track_id=14448&format=320",
+      image:
+        "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg"
+    },
+    {
+      name: "Aage Chal",
+      singer: "Raftaar",
+      path: "https://mp3.vlcmusic.com/download.php?track_id=25791&format=320",
+      image:
+        "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
+    },
+    {
+      name: "Damn",
+      singer: "Raftaar x kr$na",
+      path:
+        "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
+      image:
+        "https://filmisongs.xyz/wp-content/uploads/2020/07/Damn-Song-Raftaar-KrNa.jpg"
+    },
+    {
+      name: "Feeling You",
+      singer: "Raftaar x Harjas",
+      path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
+      image:
+        "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
+    }
+  ],
+  setConfig: function (key, value) {
+    this.config[key] = value;
+    // (2/2) Uncomment the line below to use localStorage
+    // localStorage.setItem(PlAYER_STORAGE_KEY, JSON.stringify(this.config));
+  },
+  render: function () {
+    const htmls = this.songs.map((song, index) => {
+      return `
+                        <div class="song ${
+                          index === this.currentIndex ? "active" : ""
+                        }" data-index="${index}">
+                            <div class="thumb"
+                                style="background-image: url('${song.image}')">
+                            </div>
+                            <div class="body">
+                                <h3 class="title">${song.name}</h3>
+                                <p class="author">${song.singer}</p>
+                            </div>
+                            <div class="option">
+                                <i class="fas fa-ellipsis-h"></i>
+                            </div>
+                        </div>
+                    `;
+    });
+    playlist.innerHTML = htmls.join("");
+  },
+  defineProperties: function () {
+    Object.defineProperty(this, "currentSong", {
+      get: function () {
+        return this.songs[this.currentIndex];
+      }
+    });
+  },
+  handleEvents: function () {
+    const _this = this;
+    const cdWidth = cd.offsetWidth;
+
+    
+    // Handle CD spins / stops
+    const cdThumbAnimate = cdThumb.animate([{ transform: "rotate(360deg)" }], {
+      duration: 10000, // 10 seconds
+      iterations: Infinity
+    });
+    cdThumbAnimate.pause();
+
+    
+    // Handles CD enlargement / reduction
+    document.onscroll = function () {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      const newCdWidth = cdWidth - scrollTop;
+
+      cd.style.width = newCdWidth > 0 ? newCdWidth + "px" : 0;
+      cd.style.opacity = newCdWidth / cdWidth;
+    };
+
+    
+    // Handle when click play
+    playBtn.onclick = function () {
+      if (_this.isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+    };
+
+    
+    // When the song is played
+    audio.onplay = function () {
+      _this.isPlaying = true;
+      player.classList.add("playing");
+      cdThumbAnimate.play();
+    };
+
+    
+    // When the song is pause
+    audio.onpause = function () {
+      _this.isPlaying = false;
+      player.classList.remove("playing");
+      cdThumbAnimate.pause();
+    };
+
+    
+    // When the song progress changes
+    audio.ontimeupdate = function () {
+      if (audio.duration) {
+        const progressPercent = Math.floor(
+          (audio.currentTime / audio.duration) * 100
+        );
+        progress.value = progressPercent;
+      }
+    };
+
+   
+    // Handling when seek
+    progress.onchange = function (e) {
+      const seekTime = (audio.duration / 100) * e.target.value;
+      audio.currentTime = seekTime;
+    };
+
+   
+    // When next song
+    nextBtn.onclick = function () {
+      if (_this.isRandom) {
+        _this.playRandomSong();
+      } else {
+        _this.nextSong();
+      }
+      audio.play();
+      _this.render();
+      _this.scrollToActiveSong();
+    };
+
+    
+    // When prev song
+    prevBtn.onclick = function () {
+      if (_this.isRandom) {
+        _this.playRandomSong();
+      } else {
+        _this.prevSong();
+      }
+      audio.play();
+      _this.render();
+      _this.scrollToActiveSong();
+    };
+
+   
+    // Handling on / off random song
+    randomBtn.onclick = function (e) {
+      _this.isRandom = !_this.isRandom;
+      _this.setConfig("isRandom", _this.isRandom);
+      randomBtn.classList.toggle("active", _this.isRandom);
+    };
+
+    
+    // Single-parallel repeat processing
+    repeatBtn.onclick = function (e) {
+      _this.isRepeat = !_this.isRepeat;
+      _this.setConfig("isRepeat", _this.isRepeat);
+      repeatBtn.classList.toggle("active", _this.isRepeat);
+    };
+
+    
+    // Handle next song when audio ended
+    audio.onended = function () {
+      if (_this.isRepeat) {
+        audio.play();
+      } else {
+        nextBtn.click();
+      }
+    };
+
+   
+    // Listen to playlist clicks
+    playlist.onclick = function (e) {
+      const songNode = e.target.closest(".song:not(.active)");
+
+      if (songNode || e.target.closest(".option")) {
+       
+        // Handle when clicking on the song
+        if (songNode) {
+          _this.currentIndex = Number(songNode.dataset.index);
+          _this.loadCurrentSong();
+          _this.render();
+          audio.play();
+        }
+
+        
+        // Handle when clicking on the song option
+        if (e.target.closest(".option")) {
+        }
+      }
+    };
+  },
+  scrollToActiveSong: function () {
+    setTimeout(() => {
+      $(".song.active").scrollIntoView({
+        behavior: "smooth",
+        block: "nearest"
+      });
+    }, 300);
+  },
+  loadCurrentSong: function () {
+    heading.textContent = this.currentSong.name;
+    cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
+    audio.src = this.currentSong.path;
+  },
+  loadConfig: function () {
+    this.isRandom = this.config.isRandom;
+    this.isRepeat = this.config.isRepeat;
+  },
+  nextSong: function () {
+    this.currentIndex++;
+    if (this.currentIndex >= this.songs.length) {
+      this.currentIndex = 0;
+    }
+    this.loadCurrentSong();
+  },
+  prevSong: function () {
+    this.currentIndex--;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.songs.length - 1;
+    }
+    this.loadCurrentSong();
+  },
+  playRandomSong: function () {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * this.songs.length);
+    } while (newIndex === this.currentIndex);
+
+    this.currentIndex = newIndex;
+    this.loadCurrentSong();
+  },
+  start: function () {
+    
+    // Assign configuration from config to application
+    this.loadConfig();
+
+    
+    // Defines properties for the object
+    this.defineProperties();
+
+   
+    // Listening / handling events (DOM events)
+    this.handleEvents();
+
+   
+    // Load the first song information into the UI when running the app
+    this.loadCurrentSong();
+
+    // Render playlist
+    this.render();
+
+    
+    // Display the initial state of the repeat & random button
+    randomBtn.classList.toggle("active", this.isRandom);
+    repeatBtn.classList.toggle("active", this.isRepeat);
+  }
+};
+
+app.start();
